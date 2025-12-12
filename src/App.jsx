564d92aa07c51144e8b6168b6c1d4cba1db0f3c2
Md-Gajjali <hellogajjali@gmail.com>
2025-12-12 16:31:00 +0600

    update input dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,90 +4,96 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Heading from './Componets/Heading'
 import Animation from './Componets/Animation.jsx'
-import { getDatabase, ref, set,push } from "firebase/database";
-import {  ToastContainer ,toast ,Bounce } from 'react-toastify';
+import { getDatabase, ref, set, push } from "firebase/database";
+import { ToastContainer, toast, Bounce } from 'react-toastify';
 
 
 
 
-      function App() {
+function App() {
 
 
-        const[task,setTask]=useState("")
+  const [task, setTask] = useState("")
 
-        const notify = () => {
-          task == "" ?
-          toast.error("plz inout the fill",{
-           position: "top-right",
-           autoClose: 5000,
-           hideProgressBar: false,
-           closeOnClick: false,
-           pauseOnHover: true,
-           draggable: true,
-           progress: undefined,
-           theme: "light",
-           transition: Bounce,
-         }) : toast.success('you are succses full!', {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: false,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-          transition: Bounce,
-          })
-        }
-
-
-        const handleEmail = (e)=> {
-          setTask(e.target.value);
-        }
-
-        const handleClick = (e) => {
-          e.preventDefault()
-          if (task == "") {
-            notify()
-          }else{
-             const db = getDatabase();
-             set(push(ref(db, 'Todo/'), {
-               Todos: task
-                }).than(
-                  notify(),
-                  setTask("")
-                ));
-            
-          }
-        }
-
-        return (
-
-          <>
-          <Heading />
-            <div className="relative z-10">
-            <div>
-              
-                <form className=" mx-auto rounded-4xl bg-transparent backdrop-blur-3xl px-20 py-30  w-[500px] text-center mt-20 items-center ">
-                  <div className="mb-5">
-                    <input type="text" 
-                    id="" 
-                    value={task}
-                    className="bg-neutral-secondary-medium border  border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" 
-                    placeholder="FIl the input" onChange={handleEmail} />
-                  </div>
-                  <button 
-                  type="submit" 
-                  className="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
-                  onClick={handleClick}>Submit</button>
-                </form>
+  const notify = () => {
+    task == "" ?
+      toast.error("Inter Your task", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      }) : toast.success('your task has been  successfully!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      })
+  }
 
 
+  const handleEmail = (e) => {
+    setTask(e.target.value);
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    if (task == "") {
+      notify()
+    } else {
+      const db = getDatabase();
+      set(push(ref(db, 'Todo/'), {
+        Todos: task
+      }).than(
+        notify(),
+        setTask("")
+      ));
+
+    }
+  }
+
+  return (
+
+    <>
+      <Heading />
+      <div className="relative z-10">
+        <div>
+
+          <form className=" mx-auto rounded-4xl bg-transparent backdrop-blur-3xl px-20 py-30  w-[500px] text-center mt-20 items-center ">
+            <div className="mb-5">
+              <input type="text"
+                id=""
+                value={task}
+                className="bg-neutral-secondary-medium border-none   text-heading text-sm rounded-base   block w-full px-3 py-2.5 shadow-xs placeholder:text-body"
+                placeholder="FIl the input" onChange={handleEmail} />
             </div>
+            <button
+              type="submit"
+              className="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none"
+              onClick={handleClick}>Submit</button>
+
+            <div className='mt-20'>
+              <ul className="w-full text-sm font-medium text-heading bg-neutral-primary-soft border border-default rounded-base">
+                <li className="w-full px-4 py-2 border-b border-default rounded-t-lg"></li>
+              </ul>
             </div>
-            <Animation />
-          </>
-        )
-      }
+          </form>
+
+
+        </div>
+      </div>
+      <Animation />
+    </>
+  )
+}
 
 export default App
